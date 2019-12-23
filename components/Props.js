9efa27a822cd32248a2208581b1a05fef342b407
thenhthang <quocntrivn@ymail.com>
2019-12-23 17:51:20 +0700

    Props

---
+++ components/Props.js
@@ -0,0 +1,38 @@
+import React,{Component} from 'react'
+import {View,Text, SafeAreaView} from 'react-native'
+export class User extends Component {
+    render(){
+      const user = this.props.data;
+      this.props.onReady('I am ready!');
+      return(
+        <View>
+          <Text>score: {this.props.score}</Text>
+          <Text>type: {this.props.type}</Text>  
+          <Text>Name: {user.name}</Text> 
+          <Text>Age: {user.age}</Text> 
+        </View>
+      );
+    }
+  }
+  User.defaultProps = {
+      score: 10,
+      type:'Dev'
+  }
+  var user = {name: 'foo', age: 21};
+  export class Main extends Component {
+    handleReady(str){
+      console.log(str);
+    }
+    render(){
+      return(
+        <SafeAreaView>
+        <View style={{justifyContent:'center',alignItems:'center',
+                height:'100%'}}>
+          <User type="Developer" score='20' data={user} onReady={this.handleReady}/>
+          <User type="Accountant" score='20' data={user} onReady={this.handleReady}/>
+        </View>
+        </SafeAreaView>
+        
+      );
+    }
+  }
--- index.js
+++ index.js
@@ -5,7 +5,8 @@
 import {AppRegistry} from 'react-native';
 import {Position} from './components/PositionAbsolute';
 import {UICoBan3} from './components/PositionAbsolute2';
-import {Relative} from './components/PositionRelative'
+import {Relative} from './components/PositionRelative';
+import {Main,User} from './components/Props'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Relative);
+AppRegistry.registerComponent(appName, () => Main);
